078dd4f533fc61666a6fc93115aac8e90c0e6958
着素衣 <2791023089@QQ.COM>
2026-08-31 17:01:47 +0800

    Refactor main function and add READ TABLE command

--- main.cpp
+++ main.cpp
@@ -5,27 +5,27 @@
 #include "tools/biao.h"
 #include "tools/isChinese.h"
 using namespace std;
+
 int main(){
-    // fix 编码
-    int aa = 8;// 指UTF-8
-    fixEncoding(aa);
-    string command = "";
-    ifstream file("database.db");
-    if (file.is_open()) {
-        // 还是没用
-    }
-    else {
-        // 创建数据库
+    fixEncoding(8);
+    CreateDirectoryA("data", NULL);
+    
+    // 检查数据库是否存在，不存在则创建
+    if (GetFileAttributesA("database.db") == INVALID_FILE_ATTRIBUTES) {
         createDatabase();
-        // 初始化数据库
         initDatabase();
     }
-    cout<<"欢迎使用 HhjSQL 监视器。"<<endl<<"Server version: 0.1.0-log HhjSQL Community Server (GPL)"<<endl<<"版权所有 (c) 2000, 2018, Nicesost/HHJ 或其关联公司保留所有权利。"<<endl<<endl<<"请输入命令或输入 help 获取帮助。"<<endl;
-    chonghui:
+    
+    cout<<"欢迎使用 HhjSQL 监视器。"<<endl
+        <<"Server version: 0.1.0-log HhjSQL Community Server (GPL)"<<endl
+        <<"版权所有 (c) 2000, 2018, Nicesost/HHJ 或其关联公司保留所有权利。"<<endl<<endl
+        <<"请输入命令或输入 help 获取帮助。"<<endl;
+    
     while (1){
-        
         cout<<"$ ";
+        string command;
         getline(cin, command);
+        
         if (command == "exit"){
             cout<<"byebye"<<endl;
             break;
@@ -33,34 +33,29 @@ int main(){
         else if (command == "help"){
             cout<<"CREATE TABLE: 创建表"<<endl;
             cout<<"CREATE TABLE CONTENT: 创建表内容"<<endl;
+            cout<<"READ TABLE: 读取表内容"<<endl;
             cout<<"exit: 退出程序"<<endl;
             system("pause");
         }
-
         else if (command == "CREATE TABLE"){
-            cl:
-            string tableName = "";
+            string tableName;
             system("cls");
             cout<<"请输入表名或id：";
             cin>>tableName;
-            if (containsChinese(tableName)){
+            if (containsChinese(tableName) || isAllChinese(tableName)){
                 cout<<"不能用中文！"<<endl;
-                goto cl;
+                cin.ignore();
+                continue;
             }
-            if (isAllChinese(tableName)){
-                cout<<"不能用中文！"<<endl;
-                goto cl;
+            if (createTable(tableName) == 0) {
+                cout<<"创建成功！"<<endl;
+            } else {
+                cout<<"创建失败！"<<endl;
             }
-            createTable(tableName);
-            cout<<"创建成功！"<<endl;
             Sleep(1000);
-            goto chonghui;
         }
         else if (command == "CREATE TABLE CONTENT"){
-            string tableName = "";
-            string name = "";
-            string type = "";
-            string neirong = "";
+            string tableName, name, type, neirong;
             system("cls");
             cout<<"请输入表名或id：";
             cin>>tableName;
@@ -71,18 +66,21 @@ int main(){
             cout<<"请输入字段内容：";
             cin>>neirong;
             createbiao_neirong(tableName, name, type, neirong);
-            cout<<"创建成功！"<<endl;
             Sleep(1000);
-            goto chonghui;
+        }
+        else if (command == "READ TABLE"){
+            string tableName;
+            system("cls");
+            cout<<"请输入表名：";
+            cin>>tableName;
+            readTable(tableName);
+            system("pause");
         }
         else {
             cout<<"未知命令，请输入 help 获取帮助。"<<endl;
             Sleep(1000);
-            goto chonghui;
         }
     }
-    
-
     
     return 0;
 }
